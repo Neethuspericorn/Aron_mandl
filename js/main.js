cb314ddf2bb736b526ec1fdd3ec771685de03505
niyasmn <108113550+niyasmn@users.noreply.github.com>
2024-01-19 10:32:49 +0530

    files added

---
+++ js/main.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+    $('.banner-slider').slick({
+        arrows: false,
+        dots:false,
+        fade: true
+    });
+    $('.team-slider').slick({
+			dots: true,
+			infinite: true,
+			// autoplay:true,
+			autoplaySpeed:1500,
+			arrows: true, // Display arrows
+			prevArrow: '<button type="button" class="slick-prev btn-arrow"></button>',
+			nextArrow: '<button type="button" class="slick-next btn-arrow"></button>',
+		
+			slidesToShow: 4,
+			slidesToScroll: 3,
+			responsive: [
+			  {
+				breakpoint: 1024,
+				settings: {
+				  slidesToShow: 3,
+				  slidesToScroll: 1,
+				  dots: true,
+				  infinite: true,
+			
+				}
+			  },
+			  {
+				breakpoint: 767,
+				settings: {
+				  slidesToShow: 2,
+				  dots: true,
+				  slidesToScroll: 1
+				}
+			  },
+			  {
+				breakpoint: 480,
+				settings: {
+				  slidesToShow: 1,
+				  slidesToScroll: 2
+				}
+			  }
+			  // You can unslick at a given breakpoint now by adding:
+			  // settings: "unslick"
+			  // instead of a settings object
+			]
+		  });
+  // var num = 40;
+  // $(window).bind('scroll', function () {
+  //     if ($(window).scrollTop() >= num) {
+  //         $('header').addClass('fixed-top');
+  //     } else {
+  //         $('header').removeClass('fixed-top');
+  //     }
+  // });
+});
+
+var playbutton = document.getElementById("videoPlay_btn");
+	playbutton.addEventListener("click", playPause);
+	abtVideo = document.getElementById("about_video");
+	function playPause() {
+		if (abtVideo.paused) {
+			abtVideo.play();
+			abtVideo.setAttribute('controls', 'true');
+			$('#videoPlay_btn').fadeOut();
+			playbutton.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+			}
+		else  {
+			abtVideo.pause(); 
+			$('#videoPlay_btn').css("display", "block");
+			abtVideo.removeAttribute('controls', 'true');
+			playbutton.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+			}
+	} 
+
+// document.getElementById('copyright-year').innerHTML = new Date().getFullYear();
